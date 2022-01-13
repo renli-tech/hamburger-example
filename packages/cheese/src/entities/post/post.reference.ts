af8f25c1a4b4b9e1fb7588cd500e7c4e6bffecc2
Ope Abidemi Harry <opeabidemi@gmail.com>
2022-01-13 02:16:32 +0000

    feat(cheese): cleanup

--- packages/cheese/src/entities/post/post.reference.ts
+++
@@ -1,9 +0,0 @@
-import Post from "./post.entity";
-
-export async function resolvePostReference(
-  reference: Pick<Post, "id">
-): Promise<Post | undefined> {
-  const products = await Post.findOne({ id: reference.id });
-
-  return products;
-}
